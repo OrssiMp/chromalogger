c8bd7fd6baf4eb9cdb02ba3b0077a21ef1149e07
OrssiMp <orssimpiere5@gmail.com>
2025-12-25 15:18:51 +0100

    clean

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Import and re-export the logger from chromalog.js
 export * from './modules/chromalog.js';
 
-// Set default export to be the default logger instance
 import { logger } from './modules/chromalog.js';
 export default logger;
