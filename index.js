01d2e2bb22289947b15fda7428abf6f6bf4ec7be
OrssiMp <orssimpiere5@gmail.com>
2025-12-26 10:28:04 +0100

    export browser and logger

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 export * from './modules/chromalog.js';
+export * from './modules/chromalog.browser.js';
 
 import { logger } from './modules/chromalog.js';
+import { browser } from './modules/chromalog.browser.js';
+
+export { logger, browser };
+
 export default logger;
+
